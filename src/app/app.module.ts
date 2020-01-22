ae246e9115ffff640d9c11089537d8e4b1940c5c
amauryverschooren <amauryverschooren@icloud.com>
2020-01-22 12:11:18 +0100

    primeng added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -19,6 +20,7 @@ import { UsmanComponent } from './usman/usman.component';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
     RouterModule.forRoot([
       {path: "Maxime",component:MaximeComponent},
