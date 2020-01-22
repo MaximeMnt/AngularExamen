ca679f432dc5435aeba5e1eea9796f351469c23d
xUsman <usman.shani@student.ap.be>
2020-01-22 16:25:48 +0100

    bug fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -23,6 +23,7 @@ import { UsmanComponent } from './usman/usman.component';
     BrowserModule,
     BrowserAnimationsModule,
     AppRoutingModule,
+    FormsModule,
     RouterModule.forRoot([
       {path: "Maxime",component:MaximeComponent},
       {path: "Amaury", component: AmauryComponent},
