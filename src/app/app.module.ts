797c4a50c4a58955e9b8c8bc69ef6e340c86b042
Maxime Minta <maxime.minta@hotmail.com>
2020-01-23 13:52:01 +0100

    o͓̽h͓̽ ͓̽p͓̽u͓̽l͓̽l͓̽ ͓̽m͓̽e͓̽ ͓̽d͓̽a͓̽d͓̽d͓̽y͓̽

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
-import {FormsModule} from '@angular/Forms';
-import {ButtonModule} from 'primeng/button';
-import {CardModule} from 'primeng/card';
-import {InputTextModule} from 'primeng/inputtext';
+import { FormsModule } from '@angular/Forms';
+import { ButtonModule } from 'primeng/button';
+import { CardModule } from 'primeng/card';
+import { InputTextModule } from 'primeng/inputtext';
+import {DropdownModule} from 'primeng/dropdown';
 
 
 import { AppRoutingModule } from './app-routing.module';
@@ -16,7 +17,7 @@ import { AmauryComponent } from './amaury/amaury.component';
 import { UsmanComponent } from './usman/usman.component';
 import { AngularDeel3Component } from './amaury/angular-deel3/angular-deel3.component';
 import { Angulardeel4Component } from './amaury/angulardeel4/angulardeel4.component';
-import {PanelModule} from 'primeng/panel';
+import { PanelModule } from 'primeng/panel';
 import { Maximedeel5Component } from './maxime/maximedeel5/maximedeel5.component';
 import { Angularoef2Component } from './usman/angularoef2/angularoef2.component';
 import { Usmandeel5Component } from './usman/usmandeel5/usmandeel5.component';
@@ -42,13 +43,14 @@ import { Usmandeel5Component } from './usman/usmandeel5/usmandeel5.component';
     FormsModule,
     ButtonModule,
     CardModule,
+    DropdownModule,
     InputTextModule,
     RouterModule.forRoot([
-      {path: "Maxime",component:MaximeComponent},
-      {path: "Amaury", component: AmauryComponent},
-      {path: "Usman", component: UsmanComponent},
-      {path: "", redirectTo: "Maxime", pathMatch:"full"}
-    ], {useHash:true})
+      { path: "Maxime", component: MaximeComponent },
+      { path: "Amaury", component: AmauryComponent },
+      { path: "Usman", component: UsmanComponent },
+      { path: "", redirectTo: "Maxime", pathMatch: "full" }
+    ], { useHash: true })
   ],
   providers: [],
   bootstrap: [AppComponent]
